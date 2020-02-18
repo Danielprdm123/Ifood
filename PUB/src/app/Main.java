967471832bfdaae26b561634b8028f45d34d1a91
07209702105 <07209702105@192.168.56.1>
2020-02-17 20:09:25 -0400

    será

--- PUB/src/app/Main.java
+++ PUB/src/app/Main.java
@@ -1,5 +1,16 @@
 package app;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main {
+	public static void main(String[] args) {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("sys_restaurante");
+        EntityManager manager = factory.createEntityManager();
+
+
+    }
 
 }
+ 
