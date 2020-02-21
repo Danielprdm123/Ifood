d8aa60007e7cd605970e1ed48bfdff9ee958873c
07209702105 <07209702105@192.168.56.1>
2020-02-20 22:45:27 -0400

    8_7

--- PUB/src/app/Main.java
+++ PUB/src/app/Main.java
@@ -3,13 +3,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.pub.jpaUtil.GenericDAO;
+import br.com.pub.model.Mesa;
+
 public class Main {
 	public static void main(String[] args) {
 
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("sys_restaurante");
         EntityManager manager = factory.createEntityManager();
 
-
+        GenericDAO<Mesa> m = new GenericDAO<Mesa>();
     }
 
 }
