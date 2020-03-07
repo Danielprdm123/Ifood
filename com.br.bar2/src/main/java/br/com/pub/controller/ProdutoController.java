935ec0a797c813f69a5d608c416c6a1202f570b4
Daniel <Daniel@Daniel-PC>
2020-03-07 12:57:54 -0300

    falo mais nd

--- com.br.bar2/src/main/java/br/com/pub/controller/ProdutoController.java
+++ com.br.bar2/src/main/java/br/com/pub/controller/ProdutoController.java
@@ -9,6 +9,14 @@ import br.com.pub.model.Produto;
 @ManagedBean(name = "produtoBean")
 @SessionScoped
 public class ProdutoController {
-	Produto p = new Produto();
+	Produto prod = new Produto();
+
+	public Produto getProd() {
+		return prod;
+	}
+
+	public void setProd(Produto prod) {
+		this.prod = prod;
+	}
 
 }
