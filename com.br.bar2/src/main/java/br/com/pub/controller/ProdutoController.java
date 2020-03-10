ef2591a03c0115235ca2041c774b00da388e5b72
Daniel <Daniel@Daniel-PC>
2020-03-09 21:21:43 -0300

    Psd

--- com.br.bar2/src/main/java/br/com/pub/controller/ProdutoController.java
+++ com.br.bar2/src/main/java/br/com/pub/controller/ProdutoController.java
@@ -2,9 +2,12 @@ package br.com.pub.controller;
 
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.pub.jpaUtil.GenericDAO;
 import br.com.pub.model.Produto;
@@ -15,8 +18,10 @@ public class ProdutoController implements Serializable{
 	
 
 	private static final long serialVersionUID = 1L;
-	Produto prod = new Produto();
+	private Produto prod = new Produto();
+	GenericDAO<Produto> produtoDAO = new GenericDAO<Produto>();
 
+	
 	public Produto getProd() {
 		return prod;
 	}
@@ -25,11 +30,34 @@ public class ProdutoController implements Serializable{
 		this.prod = prod;
 	}
 	
-	public void addProduto(){
+	public String addProduto(){
 	
 		GenericDAO<Produto> produtoDAO = new GenericDAO<Produto>();
+		if(prod != null) {
+
 		produtoDAO.novo(prod);
-		
+		FacesContext.getCurrentInstance().addMessage("msg: ok", FacesMessage("Salvo com sucesso"));
+		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+		return "sucesso !";
+		}
+		else {
+			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(""));
+			FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+			return "erro";
+		}
+	}
+	public List<Produto> listarProduto(){
+		return produtoDAO.litarTodos(Produto.id);
+	}
+	public String delProduto() {
+		if(prod != null) {
+
+			produtoDAO.deletar(prod);
+			FacesContext.getCurrentInstance().addMessage("msg: ok", FacesMessage("Produto Deletado"));
+			FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+			return "sucesso !";
+			}
+		return"";
 	}
 
 }
